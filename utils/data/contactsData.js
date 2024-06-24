996373e54a112d0f2a6d5656db9d7428faaed6f5
Aditya Wanwade <Adiwanwade@gmail.com>
2024-06-25 00:34:59 +0530

    Added files

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'adiwanwade@gmail.com',
+    phone: '+91 8805269160',
+    address: 'Nagpur,Maharashtra,India',
+    github: 'https://github.com/Adiwanwade',
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/aditya-wanwade-07b888216?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: "Adiwanwade"
+}
